f8fd82e2ad22025e3f41d2a23534421a03422f74
JunaidMDev <mjunaid496@gmail.com>
2021-08-30 11:34:28 +0530

    HMOS CODE

--- entry/src/ohosTest/java/comulez/github/dropindicator/ExampleOhosTest.java
+++ entry/src/ohosTest/java/comulez/github/dropindicator/ExampleOhosTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright(C) 2020-21 Application Library Engineering Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on as "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or impiles.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
 package comulez.github.dropindicator;
 
 import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
